fccfc5a3d1e23d865e8d786208789cf90ce6637d
CrispyPin <crispin@tasa.se>
2021-06-22 21:21:09 +0200

    rename brightness-wa to brightness-init wa stands for write access but init is more clear

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 cp simple-brightness /usr/bin/
 chmod 755 /usr/bin/simple-brightness
 echo "installed to /usr/bin/"
-cp brightness-wa.service /etc/systemd/system/
-systemctl enable --now brightness-wa.service
+cp brightness-init.service /etc/systemd/system/
+systemctl enable --now brightness-init.service
 
